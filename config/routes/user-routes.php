eba862afd9730a97f5b9d64964cc5d6e3c890ac3
marescanog <marescanog@gmail.com>
2021-11-02 08:22:21 +0800

    adjusted register route from get to post

--- config/routes/user-routes.php
+++ config/routes/user-routes.php
@@ -2,4 +2,4 @@
 
 $app->get("/user/check-number","UserController:is_in_DB");
 
-$app->get("/user/register","UserController:register_user");
+$app->post("/user/register","UserController:register_user");
