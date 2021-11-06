7460e55e22db7b8a0571264ac5c868a3740ee46b
marescanog <marescanog@gmail.com>
2021-11-06 12:33:22 +0800

    fixed up prod and env environment variables

--- app/Db/db.php
+++ app/Db/db.php
@@ -2,36 +2,29 @@
 
 namespace  App\Db;
 
-// use Dotenv\Dotenv;
 use PDO;
 use PDOException;
+use Dotenv\Dotenv;
 
 class DB {
     // This is the Database connection class which can be called to start a PDO connection
     public function connect(){
         // // NOTE: When running POSTman, require config code. Otherwise, comment out when pushing to prod
-        // require_once __DIR__ . '/../../vendor/autoload.php';
-        // $dotenv = Dotenv::createImmutable(__DIR__."\\..\\..\\");
-        // $dotenv->load();
-
-        // // DEVELOPMENT VARIABLES
-        // $conn_host = 'localhost';
-        // $conn_db = 'homehero';
-        // $conn_user = 'root';
-        // $conn_pass = '';
-        // $conn_charset = 'utf8mb4';
-        // $conn_dsn = "mysql:host=$conn_host;dbname=$conn_db;charset=$conn_charset";
+        require_once __DIR__ . '/../../vendor/autoload.php';
+        $dotenv = Dotenv::createImmutable(__DIR__."\\..\\..\\");
+        $dotenv->load();
 
         try{
-            // // LOCAL DATABASE, DEVELOPMENT  DATABASE CONNECTION
-            // $conn = new PDO($dsn, $user, $pass);
 
-            // PRODUCTION DATABASE CONNECTION
-            $conn = new \PDO("mysql:host=".$_ENV['DB_HOST'].";dbname=".$_ENV['DB_NAME'].";charset=utf8mb4", $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
-
-            // DEVELOPMENT DATABASE CONNECTION
-            // require_once __DIR__ . '/hidden.php';
-            // $conn = new \PDO($conn_dsn, $conn_user, $conn_pass);
+            if ($_ENV['ENVIRONMENT'] == "PROD") {
+                // PRODUCTION DATABASE CONNECTION
+                $conn = new \PDO("mysql:host=".$_ENV['DB_HOST'].";dbname=".$_ENV['DB_NAME'].";charset=utf8mb4", $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+            } 
+            
+            if ($_ENV['ENVIRONMENT'] == "DEV"){
+                // DEVELOPMENT DATABASE CONNECTION
+                $conn = new \PDO("mysql:host=".$_ENV['DB_DEV_HOST'].";dbname=".$_ENV['DB_DEV_NAME'].";charset=utf8mb4", $_ENV['DB_DEV_USERNAME'], $_ENV['DB_DEV_PASSWORD']);
+            }
 
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
