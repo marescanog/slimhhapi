d8115f78cdf8b7b373c06b4c93c8bee3636033a0
marescanog <marescanog@gmail.com>
2021-11-06 12:36:15 +0800

    production commit

--- app/Db/db.php
+++ app/Db/db.php
@@ -10,9 +10,9 @@ class DB {
     // This is the Database connection class which can be called to start a PDO connection
     public function connect(){
         // // NOTE: When running POSTman, require config code. Otherwise, comment out when pushing to prod
-        require_once __DIR__ . '/../../vendor/autoload.php';
-        $dotenv = Dotenv::createImmutable(__DIR__."\\..\\..\\");
-        $dotenv->load();
+        // require_once __DIR__ . '/../../vendor/autoload.php';
+        // $dotenv = Dotenv::createImmutable(__DIR__."\\..\\..\\");
+        // $dotenv->load();
 
         try{
 
